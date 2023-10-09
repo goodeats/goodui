de29c119e05893abda959f9c24438ea0161a52c8
Pat Needham <patrick.h.needham@gmail.com>
2023-10-09 17:55:47 -0400

    added storybook for visual design review

---
+++ src/lib/components/ui/Button/Button.stories.ts
@@ -0,0 +1,103 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Button, ButtonProps } from './Button';
+
+const meta: Meta<ButtonProps> = {
+  title: 'UI/Button',
+  component: Button,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    variant: {
+      control: {
+        type: 'select',
+        options: [
+          'default',
+          'destructive',
+          'outline',
+          'secondary',
+          'ghost',
+          'link',
+        ],
+      },
+    },
+    size: {
+      control: {
+        type: 'select',
+        options: ['default', 'sm', 'lg', 'icon'],
+      },
+    },
+    disabled: {
+      control: {
+        type: 'boolean',
+      },
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    variant: 'default',
+    children: 'Button',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    variant: 'secondary',
+    children: 'Button',
+  },
+};
+
+export const Large: Story = {
+  args: {
+    size: 'lg',
+    children: 'Button',
+  },
+};
+
+export const Small: Story = {
+  args: {
+    size: 'sm',
+    children: 'Button',
+  },
+};
+
+export const Warning: Story = {
+  args: {
+    variant: 'destructive',
+    children: 'Delete now',
+  },
+};
+
+export const Outline: Story = {
+  args: {
+    variant: 'outline',
+    children: 'Outline',
+  },
+};
+
+export const Ghost: Story = {
+  args: {
+    variant: 'ghost',
+    children: 'Ghost',
+  },
+};
+
+export const Link: Story = {
+  args: {
+    variant: 'link',
+    children: 'Link',
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+    children: 'Disabled',
+  },
+};
