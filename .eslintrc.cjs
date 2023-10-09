3fe6b73b90d168cdc7260cd82ce526076198c9d6
Pat Needham <patrick.h.needham@gmail.com>
2023-10-09 11:22:01 -0400

    Update ESLint configuration and package dependenciesCo-authored-by: bun-types <latest>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,10 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
@@ -14,5 +17,18 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/prefer-nullish-coalescing': 'off',
+    'tailwindcss/classnames-order': 'warn',
+    'tailwindcss/no-contradicting-classname': 'error',
+  },
+  settings: {
+    react: { version: 'detect' },
+  },
+  parserOptions: {
+    ecmafFeatures: { jsx: true },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
   },
 };
