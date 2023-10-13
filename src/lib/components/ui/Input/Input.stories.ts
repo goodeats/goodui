536f902e1f6ecb0a90b8880e5e9d5afb31fd29e8
Pat Needham <patrick.h.needham@gmail.com>
2023-10-12 23:08:15 -0400

    added Input component; storybook; some tests; watch storybook tests

---
+++ src/lib/components/ui/Input/Input.stories.ts
@@ -0,0 +1,97 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Input, InputProps } from './Input';
+import { InputStoryTests } from './Input.test';
+
+// TODO: stories and tests I should add:
+// - [ ] states
+// -- [ ] disabled
+// -- [ ] placeholder
+// -- [ ] value
+// - [ ] hooks
+// -- [ ] onChange
+// -- [ ] onBlur
+// -- [ ] onFocus
+// -- [ ] onKeyDown
+
+const meta: Meta<InputProps & { 'data-testid': string }> = {
+  title: 'UI/Input',
+  component: Input,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    type: {
+      control: {
+        type: 'select',
+        options: ['text', 'password', 'email', 'number', 'search', 'tel'],
+      },
+    },
+    disabled: {
+      control: {
+        type: 'boolean',
+      },
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+const testArgs = {
+  'data-testid': 'input',
+};
+
+export const Default: Story = {
+  args: {
+    ...testArgs,
+  },
+  play: (args) => InputStoryTests({ ...args }),
+};
+
+export const Password: Story = {
+  args: {
+    ...testArgs,
+    type: 'password',
+  },
+  play: (args) => InputStoryTests({ ...args, type: 'password' }),
+};
+
+export const Email: Story = {
+  args: {
+    ...testArgs,
+    type: 'email',
+  },
+  play: (args) => InputStoryTests({ ...args, type: 'email' }),
+};
+
+export const Number: Story = {
+  args: {
+    ...testArgs,
+    type: 'number',
+  },
+  play: (args) => InputStoryTests({ ...args, type: 'number' }),
+};
+
+export const Search: Story = {
+  args: {
+    ...testArgs,
+    type: 'search',
+  },
+  play: (args) => InputStoryTests({ ...args, type: 'search' }),
+};
+
+export const Tel: Story = {
+  args: {
+    ...testArgs,
+    type: 'tel',
+  },
+  play: (args) => InputStoryTests({ ...args, type: 'tel' }),
+};
+
+export const Disabled: Story = {
+  args: {
+    ...testArgs,
+    disabled: true,
+  },
+  play: (args) => InputStoryTests({ ...args, disabled: true }),
+};
---
+++ src/lib/components/ui/Input/Input.test.ts
@@ -0,0 +1,57 @@
+import { userEvent, within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+import { InputProps } from './Input';
+
+// https://storybook.js.org/docs/react/writing-tests/test-runner
+// https://storybook.js.org/docs/react/writing-tests/interaction-testing
+
+interface InputStoryTestsArgs {
+  canvasElement: HTMLElement;
+  type?: InputProps['type'];
+  disabled?: InputProps['disabled'];
+}
+
+const checkDisabled = async (input: HTMLInputElement, disabled: boolean) => {
+  if (disabled) {
+    await expect(input).toHaveAttribute('disabled');
+  } else {
+    await expect(input).not.toHaveAttribute('disabled');
+  }
+};
+
+const checkType = async (input: HTMLInputElement, type: InputProps['type']) => {
+  if (type) {
+    await expect(input).toHaveAttribute('type', type);
+  } else {
+    await expect(input).not.toHaveAttribute('type');
+  }
+};
+
+const checkInteraction = async (input: HTMLInputElement, disabled: boolean) => {
+  // focus test
+  await userEvent.type(input, 'test123');
+  if (disabled) {
+    await expect(input).not.toHaveFocus();
+  } else {
+    await expect(input).toHaveFocus();
+  }
+};
+
+const InputStoryTests = async ({
+  canvasElement,
+  type,
+  disabled = false,
+}: InputStoryTestsArgs) => {
+  const canvas = within(canvasElement);
+  const input: HTMLInputElement = canvas.getByTestId('input');
+
+  if (!input) throw new Error('Input not found');
+
+  console.log('butthead type: ', type);
+
+  await checkDisabled(input, disabled);
+  await checkType(input, type);
+  await checkInteraction(input, disabled);
+};
+
+export { InputStoryTests };
