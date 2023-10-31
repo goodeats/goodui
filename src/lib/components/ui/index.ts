ed544090e510e37be046309f4c1ced6afe8ce9be
Pat Needham <patrick.h.needham@gmail.com>
2023-10-31 17:39:53 -0400

    textarea, tooltip

--- src/lib/components/ui/index.ts
+++ src/lib/components/ui/index.ts
@@ -3,3 +3,5 @@ export * from './Checkbox/index.ts';
 export * from './DropdownMenu/index.ts';
 export * from './Input/index.ts';
 export * from './Label/index.ts';
+export * from './Textarea/index.ts';
+export * from './Tooltip/index.ts';
