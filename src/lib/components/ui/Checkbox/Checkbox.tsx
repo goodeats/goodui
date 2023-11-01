502f87e7a179445c31ec9d78ad1296493661176d
Pat Needham <patrick.h.needham@gmail.com>
2023-11-01 10:50:22 -0400

    checkbox build fix

--- src/lib/components/ui/Checkbox/Checkbox.tsx
+++ src/lib/components/ui/Checkbox/Checkbox.tsx
@@ -1,15 +1,13 @@
-'use client';
-
 import * as React from 'react';
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
 import { Check } from 'lucide-react';
 
 import { cn } from '@/lib/utils.ts';
 
-const Checkbox = React.forwardRef<
-  React.ElementRef<typeof CheckboxPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
->(({ className, ...props }, ref) => (
+const Checkbox: React.ForwardRefExoticComponent<
+  React.PropsWithoutRef<JSX.IntrinsicElements['button']> &
+    React.RefAttributes<HTMLButtonElement>
+> = React.forwardRef(({ className, ...props }, ref) => (
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
