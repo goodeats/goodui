e42b0554c417df48388fae3dd661f2d2c4a8a117
Pat Needham <patrick.h.needham@gmail.com>
2023-11-07 19:13:44 -0500

    feat: Extend Tailwind CSS with custom valuesThe commit extends the Tailwind CSS configuration with custom values from the extended theme, allowing for the use of custom colors, border radii, font sizes, and animations

--- src/lib/components/ui/Button/variants.ts
+++ src/lib/components/ui/Button/variants.ts
@@ -1,13 +1,13 @@
 import { cva } from 'class-variance-authority';
 
 export const buttonVariants = cva(
-  'inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center rounded-md text-sm font-medium outline-none ring-ring ring-offset-2 ring-offset-background transition-colors focus-within:ring-4 focus-visible:ring-4 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
+        default: 'bg-primary text-primary-foreground hover:bg-primary/80',
         destructive:
-          'bg-destructive text-destructive-foreground hover:bg-destructive/90',
+          'bg-destructive text-destructive-foreground hover:bg-destructive/80',
         outline:
           'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
         secondary:
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,44 @@
 import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { extendTailwindMerge } from 'tailwind-merge';
+import { extendedTheme } from './extended-theme.ts';
 
+// This function formats the colors from the extended theme into a format that can be used by Tailwind CSS
+function formatColors() {
+  const colors = [];
+  for (const [key, color] of Object.entries(extendedTheme.colors)) {
+    if (typeof color === 'string') {
+      colors.push(key);
+    } else {
+      const colorGroup = Object.keys(color).map((subKey) =>
+        subKey === 'DEFAULT' ? '' : subKey,
+      );
+      colors.push({ [key]: colorGroup });
+    }
+  }
+  return colors;
+}
+
+// This object extends the default Tailwind CSS configuration with custom values from the extended theme
+const customTwMerge = extendTailwindMerge({
+  theme: {
+    colors: formatColors(),
+    borderRadius: Object.keys(extendedTheme.borderRadius),
+  },
+  classGroups: {
+    'font-size': [
+      {
+        text: Object.keys(extendedTheme.fontSize),
+      },
+    ],
+    animate: [
+      {
+        animate: Object.keys(extendedTheme.animation),
+      },
+    ],
+  },
+});
+
+// This function takes a list of class values and returns a string of class names, using the custom Tailwind CSS configuration
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return customTwMerge(clsx(inputs));
 }
